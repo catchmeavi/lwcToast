d59a6b69a5e2721c7ce3fbe0ffc191debb8dcaeb
avijit chakraborty <catchmeavi@users.noreply.github.com>
2020-04-08 16:38:53 -0400

    Create testToast.js

---
+++ lwc/testToast/testToast.js
@@ -0,0 +1,17 @@
+import {LightningElement,track } from 'lwc';
+
+export default class CaseTasStipendApproval extends LightningElement {
+  /* variables for custom toast */
+    @track title;
+    @track toastmessage;
+    @track messageType;
+    @track autoClose = true;
+    /** end of variables custom toast*/
+    open(){
+      this.messageType = "error"; //this can be success, warn 
+      this.toastmessage = "Hello World.";
+      this.title = "";
+      // you can set autoclose here if you want
+      this.template.querySelector('c-lwc-custom-toast').showCustomNotice();
+    }
+}
